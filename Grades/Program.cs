b58d7ea0e0e8683e84fbc81b51150e471dee8578
keyboardbreaker <yujinost@gmail.com>
2016-05-13 16:55:27 +0100

    Calculates grades average.

---
+++ Grades/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+            book.AddGrade(75f);
+            GradeStatistics stats = book.ComputeStatistics();
+            Console.WriteLine("Average: " + stats.AverageGrade);
+            Console.WriteLine("Lowest: " + stats.LowestGrade);
+            Console.WriteLine("Highest: " + stats.HighestGrade);
+        }
+    }
+}
