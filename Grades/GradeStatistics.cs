c776cb801b9eac27823ede2e30ebe0e689c5b842
keyboardbreaker <yujinost@gmail.com>
2016-05-21 18:39:41 +0100

    Letter Grade

--- Grades/GradeStatistics.cs
+++ Grades/GradeStatistics.cs
@@ -17,5 +17,38 @@ namespace Grades
             HighestGrade = 0;
             LowestGrade = float.MaxValue;
         }
+
+        public string LetterGrade
+        {
+            get
+            {
+                string result;
+                if (AverageGrade >= 90)
+                {
+                    return "A";
+                }
+                else if (AverageGrade >= 80)
+                {
+                    result = "B";
+                }
+
+                else if (AverageGrade >= 70)
+                {
+                    result = "C";
+                }
+
+                else if (AverageGrade >= 60)
+                {
+                    result = "D";
+                }
+
+                else
+                {
+                    result = "F";
+                }
+
+                return result;
+            }
+        }
     }
 }
--- Grades/Program.cs
+++ Grades/Program.cs
@@ -19,60 +19,12 @@ namespace Grades
 
             GradeStatistics stats = book.ComputeStatistics();
 
-            //listening for a name change event.
-            book.NameChanged += OnNameChanged; //adding new items into a list of methods to invoke
-            book.NameChanged += OnNameChanged;
-            book.NameChanged += OnNameChanged2;
 
-
-            book.Name = "yjds's book";
-            WriteNames(book.Name);
             Console.WriteLine(stats.AverageGrade);
             Console.WriteLine(stats.LowestGrade);
             Console.WriteLine(stats.HighestGrade);
+            Console.WriteLine(stats.LetterGrade);
             Console.ReadKey();
-        }
-
-        private static void OnNameChanged2(object sender, NameChangedEventArgs args)
-        {
-            Console.WriteLine("***");
-        }
-
-        private static void OnNameChanged(object sender, NameChangedEventArgs args)
-        {
-            Console.WriteLine("Name changed from {0} to {1}", args.OldValue, args.NewValue);
-        }
-
-
-
-        private static void WriteBytes(float value)
-        {
-            byte[] bytes = BitConverter.GetBytes(value);
-            WriteByteArray(bytes);
-        }
-
-        private static void WriteBytes(int value)
-        {
-            byte[] bytes = BitConverter.GetBytes(value);
-            WriteByteArray(bytes);
-        }
-
-        private static void WriteByteArray(byte[] bytes)
-        {
-            foreach (byte b in bytes)
-            {
-                Console.Write("0x{0:X} ", b);
-            }
-            Console.WriteLine();
-            Console.ReadKey();
-        }
-        //dont know how many names I will have
-        private static void WriteNames(params string[] names)
-        {
-            foreach (string name in names)
-            {
-                Console.WriteLine(name);
-            }
         }
 
     }
